15d5dbe130dc3c621e59204499ecfca6eb1448ff
weblife4us <ZaQ_3501281270>
2020-01-09 20:41:48 -0500

    NotePad Initialize depend

--- src/main/java/com/note/code/controller/Hello.java
+++ src/main/java/com/note/code/controller/Hello.java
@@ -1,17 +1,17 @@
 package com.note.code.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(value = "/hello")
 
 public class Hello {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     //    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     String hello(@PathVariable String id) {
-        return "Hello, " + id + "!";
+    return "Hello, " + id + "!";
     }
 }
